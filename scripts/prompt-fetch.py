46da2b288ac6b776b6f710a8215f7460903fd19d
Phil McCluskey <phil@1092599.com>
2023-04-30 16:50:42 -0700

    added input field and fetcher function

--- scripts/prompt-fetch.py
+++ scripts/prompt-fetch.py
@@ -1,24 +1,27 @@
 import modules.scripts as scripts
+import requests
 import gradio as gr
 import os
 
 from modules import script_callbacks
 
-
+def parse_file(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        try:
+            content = response.json()
+            return content
+        except:
+            return "Error: file is not in valid JSON format"
+    else:
+        return "Error: could not download file from provided URL"
+        
 def on_ui_tabs():
+    url_input = gr.inputs.Textbox(label="Enter the URL of the .pmt file")
     with gr.Blocks(analytics_enabled=False) as ui_component:
-        with gr.Row():
-            angle = gr.Slider(
-                minimum=0.0,
-                maximum=360.0,
-                step=1,
-                value=0,
-                label="Angle"
-            )
-            checkbox = gr.Checkbox(
-                False,
-                label="Checkbox"
-            )
-        return [(ui_component, "Prompt Fetcher", "extension_template_tab")]
+        url = url_input()
+        json_data = fetch_json_data(url)
+        gr.json(json_data)
+    return [(ui_component, "Prompt Fetcher", "promtp_fetcher")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
