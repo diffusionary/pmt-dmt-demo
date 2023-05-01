77624600ce2b2b790ac1a4f80e24fbb51472f499
Phil McCluskey <phil@1092599.com>
2023-04-30 17:54:20 -0700

    Update prompt-fetch.py

--- scripts/prompt-fetch.py
+++ scripts/prompt-fetch.py
@@ -15,21 +15,18 @@ def parse_file(url):
             return "Error: file is not in valid JSON format"
     else:
         return "Error: could not download file from provided URL"
+        
+def load_prompt():
+    with gr.Column():
+        with gr.Row():
+            pmt_loc = gr.inputs.Textbox(label="Load Prompt from URL:")
+            pmt_rec = gr.inputs.Textbox(label="Results:")
+            button = gr.Button("Load", variant='primary')
+            button.click(parse_file, inputs=[pmt_loc], outputs=[ptm_rec])
 
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as ui_component:
-        with gr.Row():
-            angle = gr.Slider(
-                minimum=0.0,
-                maximum=360.0,
-                step=1,
-                value=0,
-                label="Angle"
-            )
-            checkbox = gr.Checkbox(
-                False,
-                label="Checkbox"
-            )
-        return [(ui_component, "Prompt Fetcher", "extension_template_tab")]
+        load_prompt()
+        return [(ui_component, "Prompt Loader", "extension_template_tab")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
